80fab62fc0caa4767d608290c384dd0b9faeddd7
PawelKapusta <pawelkapusta70@gmail.com>
2026-01-27 21:36:02 +0100

    chore: Update Vitest configuration to enhance testing setup with globals, jsdom environment, and coverage settings. Introduce alias resolution for improved module imports.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,22 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
+import { defineConfig } from "vitest/config";
 
-export default getViteConfig();
+export default defineConfig({
+  ...getViteConfig({}),
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+    include: ["tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    coverage: {
+      include: ["src/**/*"],
+      exclude: ["src/env.d.ts", "src/**/*.stories.{js,jsx,ts,tsx}"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
